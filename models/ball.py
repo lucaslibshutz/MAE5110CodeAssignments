3a8552869a245617e9fa7b43298543af51b7e1e2
Lucas Libshutz <lsl94@cornell.edu>
2026-08-26 13:56:01 -0400

    first full of ball

--- models/ball.py
+++ models/ball.py
@@ -3,27 +3,30 @@ import numpy as np
 def dynamics(t, state, params):
     gravity = params["gravity"]
     mass = params["mass"]
-    radius = params["radius"]
-    coeff_rest = params["coeff_of_restitution"]
+    ground_spring = params["ground_spring"]
+    ground_damping = params["ground_damping"]
 
     height = state[0]
     velocity = state[1]
 
 
     #TODO: add if statement if height is at ground, and velocity < 0, add bounce up at that instant
-    acceleration = -gravity
+    if height == 0 and velocity < 0:
+        acceleration = -gravity - (ground_spring/mass * height) - (ground_damping/mass * velocity)
+    else:
+        acceleration = -gravity
 
     state_derivative = np.array([velocity, acceleration])
+    return state_derivative
 
-    raise NotImplementedError
 
 
 def generate_params():
     params = {
         "gravity": 9.81,  # gravity m/s^2)
-        "radius": 1,  # ball radius (m)
         "mass": 1,  # point mass at end of rod (kg)
-        "coeff_of_restitution": 0.1,  # damping coefficient (kg*m^2/s)
+        "ground_spring": 1000,
+        "ground_damping": 4.5,
     }
     return params
 
